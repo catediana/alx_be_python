229d73e0d0cdfc9e32e86bf43a458bf5ce61b6be
catediana <catherinenanyala@gmail.com>
2024-12-11 11:17:22 -0800

    modified basic_operations.py file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,13 @@
-number1 = 10
-number2 =5
+# Assign predefined values to the variables
+number1 = 10 #assigning value to number1
+number2 = 5   #assigning value to number2
 
+# Perform arithmetic operations
 addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1 * number2
 
-print(f"addition of{number1}and {number2} is {addition}")
-print(f"subtraction of{number1}and {number2} is {subtraction}")
-print(f"multiplication of{number1}and {number2} is {multiplication}")
+# Print the results in the specified format
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
