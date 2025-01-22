c8d09ea933d7eab182a33fa55c958bbc85f272ed
catediana <catherinenanyala@gmail.com>
2025-01-22 10:24:34 -0800

    modified the files

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,7 +6,7 @@ class BankAccount:
     def deposit(self, amount):
         # Add the specified amount to the account balance.
         self.__account_balance += amount
-        print(f"Deposited: ${amount:.1f}")
+        print(f"Deposited: ${amount:.1f}")  # Only print the deposit message here.
 
     def withdraw(self, amount):
         # Deducting the amount from the account balance if there is sufficient funds.
