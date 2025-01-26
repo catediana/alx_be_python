e876485f789d3f3aed1250a8d548d5dd0623fe0f
catediana <catherinenanyala@gmail.com>
2025-01-26 06:01:19 -0800

    modified

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,13 +4,16 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
+
+#defining a string to format my book output
         return f"Book: {self.title} by {self.author}"
 #creating a child class of ebook ehich inerits from the Book class
 class EBook(Book):
     def __init__(self,title,author,file_size):
         super().__init__(title,author,)
         self.file_size = file_size
-       # return f"{super().__str__()} (File size: {self.file_size} MB)"
+
+#defining a string to format EBOOK  output
     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"   
 
@@ -19,10 +22,10 @@ class PrintBook(Book):
     def __init__(self,title,author,page_count):
         super().__init__(title,author) 
         self.page_count = page_count
-        #return f"{super().__str__()} (Pages: {self.page_count})"
-    def __str__(self):
-        return f"PrintBook: {self.title} by {self.author},  Page Count: {self.page_count}"   
 
+#defining a string to format my printbook output       
+     def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
  
 # implement a Library class demonstrating composition by managing a collection of books.
@@ -35,7 +38,6 @@ class Library:
         """Add a Book, EBook, or PrintBook instance to the library."""
         if isinstance(book, Book):
             self.books.append(book)
-            #print(book)
         else:
             print("Only instances of Book or its subclasses can be added.")
 
@@ -43,7 +45,5 @@ class Library:
         """Print details of each book in the library."""
         if not self.books:
             print("No books in the library.")
-           #return
-       # print("Books in Library:")
         for book in self.books:
             print(book)
